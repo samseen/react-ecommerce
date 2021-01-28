98c3f0519489871dcf852f9dd681445e916944f4
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-28 22:22:47 +0100

    Updating the Products page

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -9,11 +9,11 @@ const products = [
 const Products = () => {
     <main>
         <Grid container justify="center" spacing={4}>
-            {products.map((product)) => {
+            {products.map(product) => {
                 <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
-                    <Product />
+                    <Product product/>
                 </Grid>
-            }}
+            }
         </Grid>
     </main>
 }
