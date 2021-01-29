d237b8978ddce5e1474602e3798aac13e5b4fc61
Samson Akanbi <samson.akanbi@outlook.com>
2021-01-29 14:34:46 +0100

    Added the CardContent and Typography to the Product.jsx file.

--- src/components/Products/Product/Product.jsx
+++ src/components/Products/Product/Product.jsx
@@ -7,6 +7,22 @@ const Product = () => {
     return (
         <Card className={classes.root}>
             <CardMedia className={classes.media} image='' title={product.name}/>
+            <CardContent>
+                <div className={classes.cardContent}>
+                    <Typography variant="h5" gutterBottom>
+                        {product.name}
+                    </Typography>
+                    <Typography variant="h5">
+                        {product.price}
+                    </Typography>
+                </div>
+                <Typography variant="h5" color="textSecondary">{product.description}</Typography>
+            </CardContent>
+            <CardActions disableSpacing className={classes.cardActions}>
+                <IconButton aria-label="Add to Cart">
+                    <AddShoppingCart />
+                </IconButton>
+            </CardActions>
         </Card>
     )
 }
