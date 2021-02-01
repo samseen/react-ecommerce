8878dec091b1b675f637579dc4411e2c459fb390
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-01 23:18:50 +0100

    Created a Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
+import logo from '../../assets/commerce.png';
+import classes from '*.module.css';
 
 const Navbar = () => {
     return (
@@ -8,9 +10,17 @@ const Navbar = () => {
          <AppBar position="fixed" className={classes.appBar} color="inherit">
              <Toolbar>
                  <Typography variant="h6" className={clasess.title} color="inherit">
-                     <img src={} alt="Commerce.js" height="25px" className={classes.image} />
+                     <img src={logo} alt="Commerce.js" height="25px" className={classes.image} />
                      Commerce.js
                  </Typography>
+                 <div className={classes.grow} />
+                 <div className={classes.button}>
+                     <IconButton aria-label="Show cart items" color="inherit">
+                         <Badge badgeContent={2} color="secondary">
+                             <ShoppingCart />
+                         </Badge>
+                     </IconButton>
+                 </div>
              </Toolbar>
         </AppBar>   
         </>
