402e83d19de272744e02340ec07c5e9ac38225f3
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-04 20:14:27 +0100

    Removed the mock products.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -3,12 +3,7 @@ import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
 
-const products = [
-    { id: 1, name: 'Shoes', description: 'Running Shoes.', price: '$5', image: 'https://images.unsplash.com/photo-1503602642458-232111445657?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' },
-    { id: 2, name: 'Macbook', description: 'Apple Macbook.', price: '$5', image: 'https://images.unsplash.com/photo-1503602642458-232111445657?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80' },
-];
-
-const Products = () => {
+const Products = ({ products }) => {
     return (
         <main>
             <Grid container justify="center" spacing={4}>
