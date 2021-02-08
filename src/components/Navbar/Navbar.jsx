a48754b93c33621b28de091ebc4928911b2bb20b
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-08 07:09:05 +0100

    Displayed the cart icon only on the home page.

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Badge, MenuItem, Menu, Typography } from '@material-ui/core';
 import { ShoppingCart } from '@material-ui/icons';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import logo from '../../assets/commerce.png';
 
@@ -9,6 +9,7 @@ import useStyles from './styles';
 
 const Navbar = ({ totalItems }) => {
     const classes = useStyles();
+    const location = useLocation();
 
     return (
         <>
@@ -19,13 +20,14 @@ const Navbar = ({ totalItems }) => {
                         Commerce.js
                     </Typography>
                     <div className={classes.grow} />
+                    {location.pathname === '/' && (
                     <div className={classes.button}>
                         <IconButton component={Link} to="/cart" aria-label="Show cart items" color="inherit">
                             <Badge badgeContent={totalItems} color="secondary">
                                 <ShoppingCart />
                             </Badge>
                         </IconButton>
-                    </div>
+                    </div> )}
                 </Toolbar>
             </AppBar>
         </>
