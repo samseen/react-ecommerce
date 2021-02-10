4dcb8ca2c933689be302810c479c75203c477407
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-10 10:53:15 +0100

    Displayed the forms on the checkout page at its simplest view.

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -2,11 +2,21 @@ import React, { useState } from 'react';
 import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
 
 import useStyles from './styles';
+
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
+
 const steps = ['Shipping Address', 'Payment Details'];
 
 const Checkout = () => {
-    const [activeStep, setActiveStep] = useState(0);
+    const [activeStep, setActiveStep] = useState(2);
     const classes = useStyles();
+
+    const Confirmation = () => (
+        <div>
+            Confirmation
+        </div>
+    )
 
     const Form = () => activeStep === 0
         ? <AddressForm />
