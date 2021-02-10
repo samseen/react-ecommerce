cf51ab0fe0a98cdc7f2744c43c8b9980d8627eb3
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-10 08:34:04 +0100

    Created the Checkout Component.

---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { Paper, Stepper, Step, StepLabel, Typography, CircularProgress, Divider, Button } from '@material-ui/core';
+
+const steps = ['Shipping Address', 'Payment Details'];
+
+const Checkout = () => {
+    const [activeStep, setActiveStep] = useState(0);
+    return (
+        <>
+            <div className={classes.toolbar} />
+            <main className={classes.layout}>
+                <Paper className={classes.paper}>
+                    <Typography variant="h4" align="center">Checkout</Typography>
+                    <Stepper activeStep={0} className={classes.stepper}>
+                        {steps.map((step) => (
+                            <Step key={step}>
+                                <StepLabel>{Step}</StepLabel>
+                            </Step>
+                        ))}
+                    </Stepper>
+                </Paper>
+            </main>
+        </>
+    )
+}
+
+export default Checkout
