6d920add2ae205d8862ddcfd399be6cd2c8f5fca
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-10 10:41:16 +0100

    Created different page forms for the steppers.

--- src/components/CheckoutForm/Checkout/Checkout.jsx
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -8,6 +8,10 @@ const Checkout = () => {
     const [activeStep, setActiveStep] = useState(0);
     const classes = useStyles();
 
+    const Form = () => activeStep === 0
+        ? <AddressForm />
+        : <PaymentForm />
+
     return (
         <>
             <div className={classes.toolbar} />
@@ -21,6 +25,7 @@ const Checkout = () => {
                             </Step>
                         ))}
                     </Stepper>
+                    { activeStep === steps.length ? <Confirmation /> : <Form /> }
                 </Paper>
             </main>
         </>
