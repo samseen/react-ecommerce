0dd44cacd8ca5703802bad674c0e517dd74806ec
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-11 07:27:47 +0100

    Created a token from the cart.

--- src/components/CheckoutForm/AddressForm.jsx
+++ src/components/CheckoutForm/AddressForm.jsx
@@ -1,17 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { InputLabel, Select, MenuItem, Button, Grid, Typography } from '@material-ui/core';
 import { useForm, FormProvider } from 'react-hook-form';
 
+import { commerce } from '../../lib/commerce';
+
+import FormInput from './CustomTextField';
+
 const AddressForm = () => {
+    const [shippingCountries, setShippingCountries] = useState([]);
+    const [shippingCountry, setShippingCountry] = useState('');
+    const [shippingSubdivisions, setShippingSubdivisions] = useState([]);
+    const [shippingSubdivision, setShippingSubdivision] = useState('');
+    const [shippingOptions, setShippingOptions] = useState([]);
+    const [shippingOption, setShippingOption] = useState('');
     const methods = useForm();
+
+    const fetchShippingCountries = async(checkoutTokenId) => {
+        const { countries } = await commerce.services.localeListShippingCountries(checkoutTokenId);
+
+        setShippingCountries(countries);
+    }
 
     return (
         <>
             <Typography variant="h6" gutterBottom>Shipping Address</Typography>
             <FormProvider {...methods}>
-                <form onSubmit={}>
+                <form onSubmit=''>
                     <Grid container spacing={3}>
-                        
+                        <FormInput required name="firstName" label="First Name" />
+                        <FormInput required name="lastName" label="Last Name" />
+                        <FormInput required name="address" label="Address" />
+                        <FormInput required name="email" label="Email" />
+                        <FormInput required name="city" label="City" />
+                        <FormInput required name="zip" label="ZIP / Postal Code" />
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Country</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem key={} value={}>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Subdivision</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem key={} value={}>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <InputLabel>Shipping Options</InputLabel>
+                            <Select value={} fullWidth onChange={}>
+                                <MenuItem key={} value={}>
+                                    Select Me
+                                </MenuItem>
+                            </Select>
+                        </Grid>
                     </Grid>
                 </form>
             </FormProvider>
