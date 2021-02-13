37b87cee93260e94f4743488e841d5ac5527aa25
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-13 16:33:53 +0100

    Added the order, onCaptureCheckout and error parameters to the Checkout component.

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ const App = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState({});
     const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -53,8 +54,9 @@ const App = () => {
             const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
 
             setOrder(incomingOrder);
+            refreshCart();
         } catch (error) {
-
+            setErrorMessage(error.data.error.message);
         }
     }
 
@@ -83,7 +85,12 @@ const App = () => {
                     />
                 </Route>
                 <Route exact path="/checkout">
-                    <Checkout cart={cart} />
+                    <Checkout 
+                        cart={cart} 
+                        order={order}
+                        onCaptureCheckout={handleCaptureCheckout}
+                        error={errorMessage}
+                    />
                 </Route>
             </Switch>
         </div>   
