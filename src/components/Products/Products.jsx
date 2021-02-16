7ad6eaf8802674c047cee27a275a206c6db9662c
Samson Akanbi <samson.akanbi@outlook.com>
2021-02-16 08:18:42 +0100

    Used Material Design for the Products page.

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { CssBaseline, Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
 
 const Products = ({ products, onAddToCart }) => {
     return (
         <main>
+            <CssBaseline />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
